763de31227c0316ff95722fc87d27f01d7659bf3
scpmrh <sean.reags@gmail.com>
2021-01-27 16:36:14 -0500

    added angular material

--- trailtracker/src/app/app.module.ts
+++ trailtracker/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router'
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { LoginComponent } from './login/login.component';
         path: '',
         component: HomeComponent
       }
-    ])
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
