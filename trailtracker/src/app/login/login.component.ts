958e5f2b6a289eedb1270ac7247c2925c9263269
Asher <lisonfire@gmail.com>
2021-01-27 18:24:24 -0500

    working on it

--- trailtracker/src/app/login/login.component.ts
+++ trailtracker/src/app/login/login.component.ts
@@ -1,10 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { AppComponent } from 'app/app.component';
 
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [MatButtonModule, MatFormFieldModule],
+  providers: [],
+  bootstrap: [AppComponent]
+
+}) export class AppModule { }
+
+
+//const modules = [
+//  MatButtonModule,
+//  MatFormFieldModule,
+//];
+/*
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
 export class LoginComponent implements OnInit {
 
   constructor() { }
@@ -12,4 +33,4 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
-}
+} */
