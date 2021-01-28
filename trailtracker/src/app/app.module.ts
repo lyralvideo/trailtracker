186891baef91560c96a94093d1d1e3593e5ce4fd
Ryan Schneider <schneiderryan2017@gmail.com>
2021-01-28 15:08:23 -0500

    fixes

--- trailtracker/src/app/app.module.ts
+++ trailtracker/src/app/app.module.ts
@@ -7,15 +7,12 @@ import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ResultsComponent } from './results/results.component';
 import { TrailComponent } from './trail/trail.component';
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms'; 
 import { NgSelectModule } from '@ng-select/ng-select'; 
-=======
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
->>>>>>> 0f5f5db53955a474417e6bcf844c410ea63881a9
 
 @NgModule({
   declarations: [
