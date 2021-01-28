a75bc211b083a5679860275f2f1a67bc1e6f84e9
Thomas Yoder <yoder.394@osu.edu>
2021-01-28 07:17:17 -0500

    implemented search results page

--- trailtracker/src/app/results/results.component.ts
+++ trailtracker/src/app/results/results.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {MatCardHarness} from '@angular/material/card/testing';
+
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
