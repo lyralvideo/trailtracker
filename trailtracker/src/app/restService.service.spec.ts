5501dd0a2b57b488440ad5825ccb28574cbc511e
Asher <lisonfire@gmail.com>
2021-02-13 12:24:39 -0500

    Fixes testing enviroment for HTTPClient

--- trailtracker/src/app/restService.service.spec.ts
+++ trailtracker/src/app/restService.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 
 import { RestService } from './restService.service';
 
@@ -6,7 +7,8 @@ describe('RestService', () => {
   let service: RestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [HttpClientTestingModule],});
+    
     service = TestBed.inject(RestService);
   });
 
