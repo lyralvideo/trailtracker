525bae62a1464d2fb0cd7c78fd7ff126be709613
Asher <lisonfire@gmail.com>
2021-02-14 14:26:12 -0500

    Fixing testing enviroment

--- trailtracker/src/app/results/results.component.spec.ts
+++ trailtracker/src/app/results/results.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import { ResultsComponent } from './results.component';
 
 describe('ResultsComponent', () => {
@@ -7,6 +7,7 @@ describe('ResultsComponent', () => {
   let fixture: ComponentFixture<ResultsComponent>;
 
   beforeEach(async () => {
+    TestBed.configureTestingModule({imports: [HttpClientTestingModule],});
     await TestBed.configureTestingModule({
       declarations: [ ResultsComponent ]
     })
