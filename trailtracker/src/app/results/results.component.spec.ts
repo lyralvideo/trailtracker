466e7235f9885dfa6bfa59a05e36e4dc775daba8
Asher <lisonfire@gmail.com>
2021-02-18 15:45:41 -0500

    Test enviroment fixes

--- trailtracker/src/app/results/results.component.spec.ts
+++ trailtracker/src/app/results/results.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import { ResultsComponent } from './results.component';
+import { RouterTestingModule} from '@angular/router/testing';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 describe('ResultsComponent', () => {
   let component: ResultsComponent;
@@ -15,6 +17,13 @@ describe('ResultsComponent', () => {
   });
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ NgSelectModule,
+        RouterTestingModule.withRoutes(
+          [{path: '', component: ResultsComponent}]
+        )
+      ]
+    });
     fixture = TestBed.createComponent(ResultsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- trailtracker/src/app/trail/trail.component.spec.ts
+++ trailtracker/src/app/trail/trail.component.spec.ts
@@ -1,12 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import { TrailComponent } from './trail.component';
+import { RouterTestingModule} from '@angular/router/testing';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 describe('TrailComponent', () => {
   let component: TrailComponent;
   let fixture: ComponentFixture<TrailComponent>;
 
   beforeEach(async () => {
+    TestBed.configureTestingModule({imports: [HttpClientTestingModule],});
     await TestBed.configureTestingModule({
       declarations: [ TrailComponent ]
     })
@@ -14,6 +17,13 @@ describe('TrailComponent', () => {
   });
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ NgSelectModule,
+        RouterTestingModule.withRoutes(
+          [{path: '', component: TrailComponent}]
+        )
+      ]
+    });
     fixture = TestBed.createComponent(TrailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
